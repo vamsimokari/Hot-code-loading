b1ccbc9cc0656f1fd951328b436edf6fe82e1db6
vamsimokari <vamsimokari@gmail.com>
2015-11-08 21:45:54 +0530

    Update test2.erl

--- test2.erl
+++ test2.erl
@@ -1,15 +1,19 @@
 -module(test2).
 -compile(export_all).
+%start(Node)=Pid()
+%Node=atom(), The node where print-process shall be started,
+%%starts a print process and returns the process id of newly
+%% created process
 start(Node)->
         spawn(Node,test2,loop,[]).
 
 loop()->
         receive
-                %%switch->
-                        test2:loop();
-                %%hii->
-                        erlang:display([self(),hello]),
-                        loop();
+%%                switch->
+%%                        test2:loop();
+%%                hii->
+%%                       erlang:display([self(),hello]),
+%5                       loop();
                 hello->
                         erlang:display([self(),hiii]),
                         loop();
