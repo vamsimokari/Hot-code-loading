9ea3fa45c8dc7d978c741875a0553ec1a6108038
vamsimokari <vamsimokari@gmail.com>
2015-11-09 15:41:27 +0530

    Update test2.erl

--- test2.erl
+++ test2.erl
@@ -9,14 +9,16 @@ start(Node)->
 
 loop()->
         receive
-%%                switch->
+%%              switch->
 %%                        test2:loop();
 %%                hii->
 %%                       erlang:display([self(),hello]),
 %5                       loop();
-                hello->
-                        erlang:display([self(),hiii]),
-                        loop();
+        
+        %I am adding one more clause to receive loop here switch is an atom what i make here is tail recursive to the 
+        %loop function but if you see that i specified module name as well ,what this actually means if i make a call to function
+        %sepecified nodule name as well ,I tell erlang to use latest version of the module ,here we force to specify use the 
+        %latest beam 
                 Anything->  
                         erlang:display([self(),Anything]),
                 loop()
